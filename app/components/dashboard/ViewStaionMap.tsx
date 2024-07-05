f3fbf931eecfa2510a2cd8aa134c26919fb696a6
MacBook <macbook@MacBooks-MacBook-Air.local>
2024-07-05 13:58:06 +0700

    fix:debug stucture v13

--- app/components/dashboard/ViewStaionMap.tsx
+++ app/components/dashboard/ViewStaionMap.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ViewStaionMap() {
   return (
-    <div className='flex flex-col justify-around items-center w-[270px] p-2  bg-white rounded-md'>
+    <div className='flex flex-col justify-around items-center w-[260px] p-2  bg-white rounded-md'>
       <img src="images/map_station.png" className='px-2' alt="" />
       <div className='w-full bg-[#C8D8F4] h-[1px] my-2'/>
       <div className='flex justify-end items-center w-full'>
