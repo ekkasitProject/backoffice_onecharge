c4b86f5e7fb4503fc00a5ebc0bbbf03e2e9b3a4d
MacBook <macbook@MacBooks-MacBook-Air.local>
2024-07-05 13:28:29 +0700

    fix:debug stucture v10

--- app/components/dashboard/TotalCharge.tsx
+++ app/components/dashboard/TotalCharge.tsx
@@ -133,7 +133,7 @@ const TotalCharge = () => {
   };
 
   return (
-    <div className="w-[480px]  bg-white px-4 py-2 rounded-md">
+    <div className="w-[450px]  bg-white px-4 py-2 rounded-md">
       <div>
         <span className="text-[#364A63] text-[13px] mr-2 font-medium">
           Total Charge Session
